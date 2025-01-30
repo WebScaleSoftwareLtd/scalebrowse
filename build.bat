075ad713c5c1eab9e8df826c2ecb018ddfd7e7c0
Astrid Gealer <astrid@gealer.email>
2025-01-30 18:40:22 +0000

    oops extra slash

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 cd source
-set PATH=%PATH%;%cd%\chromium\third_party\depot_tools\
+set PATH=%PATH%;%cd%\chromium\third_party\depot_tools
 set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 python3 scripts/runhooks.py
 ninja -C out/Release vivaldi
